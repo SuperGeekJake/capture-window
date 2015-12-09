2193c4058eb77529ac9a99737cbe2ca601a78863
Jacob Williams <jake@jacobwilliams.me>
2015-12-09 13:28:53 -0800

    [WIP] Built basic window with ability to set size

---
+++ window.js
@@ -0,0 +1,37 @@
+const $ = require('jquery');
+const remote = require('remote');
+
+var win = remote.getCurrentWindow();
+var $dimensionsContainer = $('#dimensions');
+
+function getWindowDimensions() {
+	var bounds = win.getBounds();
+	return `${bounds.width} x ${bounds.height}`;
+}
+
+// Calculate window dimensions and create element
+$dimensionsContainer.html(getWindowDimensions());
+
+$(window).resize(function() {
+	console.log('[NOTICE] Window resized.');
+  $dimensionsContainer.html(getWindowDimensions());
+});
+
+$dimensionsContainer.keyup(function (event) {
+	if (event.keyCode !== 13) {
+		return;
+	}
+
+	event.preventDefault();
+
+	var content = $(this).html().split('x', 2);
+	var int1 = parseInt(content[0].replace(/[^\d.]/g, ''));
+	var int2 = parseInt(content[1].replace(/[^\d.]/g, ''));
+
+	win.setSize(int1, int2);
+	console.log('[NOTICE] Dimension input changed.');
+});
+
+$('#reset').click(function () {
+	win.setSize(960, 540);
+});

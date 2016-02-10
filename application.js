b54305846e2bd226b4bd6aa15f0eee2d7205b225
Jacob Williams <jake@jacobwilliams.me>
2016-02-10 07:47:28 -0800

    Implement material design icons

--- application.js
+++ application.js
@@ -4,8 +4,8 @@
 'use strict';
 
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const app = electron.app; // Module to control application life.
+const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
 
 // Report crashes to our server.
 electron.crashReporter.start();
